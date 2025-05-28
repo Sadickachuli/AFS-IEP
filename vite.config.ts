178a4c2afbff660a4902e1f63034f53933a1a358
Sadickachuli <m.achuli@alustudent.com>
2025-05-28 10:21:32 -0700

    run build

--- vite.config.ts
+++ vite.config.ts
@@ -30,5 +30,16 @@ export default defineConfig({
     sourcemap: true,
     minify: 'esbuild',
     chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled']
+        }
+      }
+    }
   },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom']
+  }
 }) 
